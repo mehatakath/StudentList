ade3cbd5b46313f81205e34f6b14b0a957e252aa
mehatakat <mehtakat@sheridanc.on.ca>
2020-02-20 09:48:48 -0500

    New class - graduate student

---
+++ src/studentlist/GraduateStudent.java
@@ -0,0 +1,26 @@
+ /*
+        [Class name]
+        Author: [the name of the person who created the class. It might be different from yours]
+        Date: [date]
+
+        Description
+        [Description about the class]
+    */
+
+package studentlist;
+
+/**
+ *
+ * @author Katrina
+ */
+public class GraduateStudent extends Student {
+    
+    private String rank;
+    
+        public GraduateStudent(String name, String rank){
+        
+        super(name);
+        this.rank=rank;
+        
+    }
+}
